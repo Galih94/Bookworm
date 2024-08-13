f1cbb0c06b8bca0bad1c383ca2bfc18d616606bd
galih <samudragalih94@gmail.com>
2024-08-13 16:03:49 +0700

    clean code

--- Bookworm/Bookworm/ContentView.swift
+++ Bookworm/Bookworm/ContentView.swift
@@ -7,14 +7,8 @@
 import SwiftUI
 
 struct ContentView: View {
-    @AppStorage("notes") private var notes = ""
     var body: some View {
-        NavigationStack {
-            TextField("Enter your text", text: $notes, axis: .vertical)
-                .textFieldStyle(.roundedBorder)
-                .navigationTitle("Notes")
-                .padding()
-        }
+        Text("Hello world")
     }
 }
 
