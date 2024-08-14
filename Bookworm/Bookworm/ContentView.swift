0349e9bfabd4b5a70f44cba99be6bbf1ee8da5db
galih <samudragalih94@gmail.com>
2024-08-14 10:05:28 +0700

    feat(SwiftData): implement Query all SwiftData model Student Array and show it in UI

--- Bookworm/Bookworm/ContentView.swift
+++ Bookworm/Bookworm/ContentView.swift
@@ -4,11 +4,20 @@
 //
 //  Created by Galih Samudra on 02/08/24.
 //
+import SwiftData
 import SwiftUI
 
 struct ContentView: View {
+    
+    @Query var students: [Student]
+    
     var body: some View {
-        Text("Hello world")
+        NavigationStack {
+            List(students) { student in
+                Text(student.name)
+            }
+            .navigationTitle("Classroom")
+        }
     }
 }
 
