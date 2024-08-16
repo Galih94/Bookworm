7811283db6a7e3f75855a29085c61fc8d9a924f8
galih <samudragalih94@gmail.com>
2024-08-16 11:29:45 +0700

    feat(AddBook): create simple Form UI to input new book

---
+++ Bookworm/Bookworm/AddBookView.swift
@@ -0,0 +1,54 @@
+//
+//  AddBookView.swift
+//  Bookworm
+//
+//  Created by Galih Samudra on 16/08/24.
+//
+
+import SwiftUI
+
+struct AddBookView: View {
+    @Environment(\.modelContext) var modelContext
+    
+    @State private var title = ""
+    @State private var author = ""
+    @State private var rating = 3
+    @State private var genre = "Fantasy"
+    @State private var review = ""
+    
+    let genres = ["Fantasy", "Horror", "Kids", "Mistery", "Poetry", "Romance", "Thriller"]
+    var body: some View {
+        NavigationStack {
+            Form {
+                Section {
+                    TextField("Name of book", text: $title)
+                    TextField("Author's name", text: $author)
+                    
+                    Picker("Genre", selection: $genre) {
+                        ForEach(genres, id: \.self) {
+                            Text($0)
+                        }
+                    }
+                }
+                
+                Section("Write Review") {
+                    TextEditor(text: $review)
+                    Picker("Rating", selection: $rating) {
+                        ForEach(0..<6) { rate in
+                            Text("\(rate)")
+                        }
+                    }
+                }
+                
+                Section {
+                    Button("Save") {}
+                }
+            }
+            .navigationTitle("Add Book")
+        }
+    }
+}
+
+#Preview {
+    AddBookView()
+}
