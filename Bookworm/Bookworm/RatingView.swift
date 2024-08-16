79f6e29837dd94dc2ca48655d567fe1ee5c7ff73
galih <samudragalih94@gmail.com>
2024-08-16 17:29:35 +0700

    feat(UI): create `RatingView`

---
+++ Bookworm/Bookworm/RatingView.swift
@@ -0,0 +1,48 @@
+//
+//  RatingView.swift
+//  Bookworm
+//
+//  Created by Galih Samudra on 16/08/24.
+//
+
+import SwiftUI
+
+struct RatingView: View {
+    @Binding var rating: Int
+    
+    var label = ""
+    var maxRating = 6
+    var offImage: Image?
+    var onImage = Image(systemName: "star.fill")
+    var offColor = Color.gray
+    var onColor = Color.yellow
+    var body: some View {
+        HStack {
+            if label.isEmpty == false {
+                Text(label)
+            }
+            
+            ForEach(1..<maxRating, id: \.self) { numb in
+                Button(role: nil) {
+                    rating = numb
+                } label: {
+                    image(for: numb)
+                        .foregroundStyle(numb > rating ? offColor : onColor)
+                }
+
+            }
+        }
+    }
+    
+    func image(for number: Int) -> Image {
+        if number > rating {
+            return offImage ?? onImage
+        } else {
+            return onImage
+        }
+    }
+}
+
+#Preview {
+    RatingView(rating: .constant(3))
+}
