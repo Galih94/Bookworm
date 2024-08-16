2cbffc6458b8c2789a8db42775ddca4076328fef
galih <samudragalih94@gmail.com>
2024-08-16 13:56:55 +0700

    feat(UI): create UI to show Add Book Screen and count saved book review

--- Bookworm/Bookworm/ContentView.swift
+++ Bookworm/Bookworm/ContentView.swift
@@ -4,11 +4,29 @@
 //
 //  Created by Galih Samudra on 02/08/24.
 //
+import SwiftData
 import SwiftUI
 
 struct ContentView: View {
+    @Environment(\.modelContext) var modelContext
+    @Query var books: [Book]
+    @State private var showingAddScreem = false
+    
     var body: some View {
-        Text("Hello world")
+        NavigationStack{
+            Text("Count: \(books.count)")
+                .navigationTitle("Bookworm")
+                .toolbar(content: {
+                    ToolbarItem(placement: .topBarTrailing) {
+                        Button("Add Book", systemImage: "plus") {
+                            showingAddScreem.toggle()
+                        }
+                    }
+                })
+                .sheet(isPresented: $showingAddScreem, content: {
+                    AddBookView()
+                })
+        }
     }
 }
 
