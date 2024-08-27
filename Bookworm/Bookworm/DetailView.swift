86dbad9f588a9757e4c7972abc73f9af4a7580df
galih <samudragalih94@gmail.com>
2024-08-27 10:40:54 +0700

    feat(UI): create UI detail rating view

---
+++ Bookworm/Bookworm/DetailView.swift
@@ -0,0 +1,55 @@
+//
+//  DetailView.swift
+//  Bookworm
+//
+//  Created by Galih Samudra on 27/08/24.
+//
+
+import SwiftUI
+import SwiftData
+
+struct DetailView: View {
+    let book: Book
+    var body: some View {
+        ScrollView {
+            ZStack(alignment: .bottomTrailing) {
+                Image(book.genre)
+                    .resizable()
+                    .scaledToFit()
+                Text(book.genre.uppercased())
+                    .font(.caption)
+                    .fontWeight(.black)
+                    .padding(8)
+                    .foregroundStyle(.white)
+                    .background(.black.opacity(0.75))
+                    .clipShape(.capsule)
+                    .offset(x: -5, y: -5)
+                
+            }
+            Text(book.author)
+                .font(.title)
+                .foregroundStyle(.secondary)
+            Text(book.review)
+                .padding()
+            RatingView(rating: .constant(book.rating))
+                .font(.largeTitle)
+            
+        }
+        .navigationTitle(book.title)
+        .navigationBarTitleDisplayMode(.inline)
+        .scrollBounceBehavior(.basedOnSize)
+    }
+}
+
+#Preview {
+    do {
+        let config = ModelConfiguration(isStoredInMemoryOnly: true)
+        let container = try ModelContainer(for: Book.self, configurations: config)
+        let example = Book(title: "Test Book", author: "Test Author", genre: "Fantasy", review: "Test Review", rating: 3)
+        return DetailView(book: example)
+            .modelContainer(container)
+    } catch {
+        return Text("error preview: \(error.localizedDescription)")
+    }
+    
+}
