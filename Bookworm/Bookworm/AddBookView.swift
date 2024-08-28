72e83df5266158614274142395dac9d79a88a91e
galih <samudragalih94@gmail.com>
2024-08-28 15:43:43 +0700

    feat(Validation): add validation add new review

--- Bookworm/Bookworm/AddBookView.swift
+++ Bookworm/Bookworm/AddBookView.swift
@@ -16,8 +16,30 @@ struct AddBookView: View {
     @State private var rating = 3
     @State private var genre = "Fantasy"
     @State private var review = ""
+    @State private var showAlert = false
+    @State private var alertMsg = ""
     
     let genres = ["Fantasy", "Horror", "Kids", "Mistery", "Poetry", "Romance", "Thriller"]
+    private func saveBook() {
+        if title.isEmpty {
+            showAlert = true
+            alertMsg = "Book's title is empty, please fill the book's title"
+            return
+        } else if author.isEmpty {
+            showAlert = true
+            alertMsg = "Book's author is empty, please fill the book's author"
+            return
+        } else if review.isEmpty {
+            showAlert = true
+            alertMsg = "Book's review is empty, please fill the book's review"
+            return
+        }
+        
+        let newBook = Book(title: title, author: author, genre: genre, review: review, rating: rating)
+        modelContext.insert(newBook)
+        dismiss()
+    }
+    
     var body: some View {
         NavigationStack {
             Form {
@@ -39,13 +61,17 @@ struct AddBookView: View {
                 
                 Section {
                     Button("Save") {
-                        let newBook = Book(title: title, author: author, genre: genre, review: review, rating: rating)
-                        modelContext.insert(newBook)
-                        dismiss()
+                        saveBook()
                     }
                 }
             }
             .navigationTitle("Add Book")
+            .alert("Failed to save", isPresented: $showAlert) {
+                Button("OK") {}
+            } message: {
+                Text(alertMsg)
+            }
+
         }
     }
 }
