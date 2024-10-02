64875d7a5b016cf40b371f889a603252c773b776
galih <samudragalih94@gmail.com>
2024-10-02 11:37:04 +0700

    feat(VoiceOver): That groups all its children together, applies the label “Rating”, but then adds a value based on the current stars. It also allows that rating value to be adjusted up or down using swipes, which is much better than trying to work through lots of individual images.

--- Bookworm/Bookworm/RatingView.swift
+++ Bookworm/Bookworm/RatingView.swift
@@ -29,12 +29,24 @@ struct RatingView: View {
                     image(for: numb)
                         .foregroundStyle(numb > rating ? offColor : onColor)
                 }
-                .accessibilityLabel("\(numb == 1 ? "1 star" : "\(numb) stars")")
-                .accessibilityAddTraits(numb > rating ? [] : [.isSelected])
 
             }
         }
         .buttonStyle(.plain)
+        .accessibilityElement()
+        .accessibilityLabel(label)
+        .accessibilityValue("\(rating == 1 ? "1 star" : "\(rating) stars")")
+        .accessibilityAdjustableAction { direction in
+            switch direction {
+            case .increment:
+                if rating < maxRating { rating += 1 }
+            case .decrement:
+                if rating > 1 { rating -= 1 }
+            default:
+                break
+            }
+        }
+
     }
     
     func image(for number: Int) -> Image {
